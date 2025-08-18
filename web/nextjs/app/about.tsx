a44ea3ba477989065ae2c1ed0e909e37fa9cafe4
ydming <xm95zz@gmail.com>
2025-08-18 16:26:14 +0800

    update next

---
+++ web/nextjs/app/about.tsx
@@ -0,0 +1,5 @@
+function About() {
+  return <div>Hello, Next.js!</div>
+}
+
+export default About
---
+++ web/nextjs/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+	return (
+		<div className="">
+            <div className="">nextjs</div>
+            <button className="btn btn btn-primary">去about</button>
+		</div>
+	);
+}
