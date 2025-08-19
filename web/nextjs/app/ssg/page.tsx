2e842637c89655f6600a195ca8f2d9ca4e50d9c5
ydming <xm95zz@gmail.com>
2025-08-19 17:05:11 +0800

    update next

---
+++ web/nextjs/app/ssg/page.tsx
@@ -0,0 +1,38 @@
+import { GetStaticProps } from "next";
+
+type Props = {
+	id: number;
+	name: string;
+	completed: boolean;
+};
+
+// export const getStaticProps: GetStaticProps= async () => {
+// 	const res = await fetch("http://localhost:3000/data.json");
+// 	const data: Props[] = await res.json();
+// 	return { props: { data } };
+// };
+
+// type HomeProps = {
+// 	data: Props[];
+// };
+
+export default async function Home() {
+	const res = await fetch("http://localhost:3000/data.json");
+	const data: Props[] = await res.json();
+
+	return (
+		<div className="my-ssglist">
+			{data.map((item) => {
+				return (
+					<div key={item.id}>
+						<input
+							type="checkbox"
+							defaultChecked={item.completed}
+						/>
+						{item.name}
+					</div>
+				);
+			})}
+		</div>
+	);
+}
