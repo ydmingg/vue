63f0d64e7de4efe4cd87dd81d207ab7d861e009a
ydming <xm95zz@gmail.com>
2025-08-19 16:24:46 +0800

    update next

--- web/nextjs/app/page.tsx
+++ web/nextjs/app/page.tsx
@@ -1,11 +1,49 @@
+import { GetServerSideProps } from "next";
 import Link from "next/link";
 
-export default function Home() {
+type Props = {
+    id: number;
+    name: string;
+    completed: boolean;
+};
+
+// export const getServerSideProps: GetServerSideProps= async () => {
+//     // 读文件的方式获取数据
+//     const fs = await import("fs");
+//     const data1 = fs.readFileSync("data.json", "utf-8");
+//     const data2 = JSON.parse(data1);
+//     // return { props: { data: data2 } };
+
+//     // 服务器请求数据
+//     const res = await fetch("http://localhost:3000/data.json");
+//     const data: Props[] = await res.json();
+    
+//     return { props: { data } };
+// };
+
+// type HomeProps = {
+//     data: Props[];
+// };
+export default async function Home() {
+    const res = await fetch("http://localhost:3000/data.json");
+    const data: Props[] = await res.json();
+
 	return (
 		<div className="">
             <div className="">nextjs</div>
             <Link href="/blog">lisa</Link>
             <a href="/blog">hello world</a>
+            {data.map((item) => {
+				return (
+					<div key={item.id}>
+						<input
+							type="checkbox"
+							defaultChecked={item.completed}
+						/>
+						{item.name}
+					</div>
+				);
+			})}
 		</div>
 	);
 }
--- web/react/src/App.tsx
+++ web/react/src/App.tsx
@@ -1,8 +1,51 @@
+import { useEffect, useState } from "react";
 import ReatBase from "../reatBase/index";
+
+type Props = {
+    id: number;
+    name: string;
+    completed: boolean;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function Data() { 
+    console.log(ReatBase);
+    const [data, setData] = useState<Props[]>([])
+    
+    useEffect(() => {
+        console.log("effect");
+        
+        (async () => { 
+            const res = await fetch("/data.json");
+            const data: Props[]= await res.json();
+            setData(data);
+
+            
+        })()
+    
+    }, []);
+
+
+    return (
+        <>
+            {data.map((item) => { 
+                return (
+                    <div key={item.id}>
+                        <input type="checkbox" defaultChecked={item.completed} />
+                        {item.name}
+                    </div>
+                )
+            })}
+        </>
+    )
+}
 
 function App() { 
     return (
-        <ReatBase />
+        <>
+            <ReatBase />
+            {/* <Data></Data> */}
+        </>
     )
 }
 
